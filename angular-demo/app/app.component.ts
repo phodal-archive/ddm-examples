de7ae7bfe79e399291209e54cbf3f56f5ca366f2
Fengda HUANG <h@phodal.com>
2016-06-23 21:59:42 +0800

    [angualr2] try to add ajax

--- angular-demo/app/app.component.ts
+++ angular-demo/app/app.component.ts
@@ -1,7 +1,12 @@
 import {Component} from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(http:Http) {
+    
+  }
+}
